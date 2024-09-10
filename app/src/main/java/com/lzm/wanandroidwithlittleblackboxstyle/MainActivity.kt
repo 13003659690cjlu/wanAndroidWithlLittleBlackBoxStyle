9908f3e6c59ee85a5bc3ef7dc6dd62cd42c394eb
13003659690cjlu <89671965+13003659690cjlu@users.noreply.github.com>
2024-09-10 23:03:57 +0800

    <add>[ui]:实现底部按钮动态加载

--- app/src/main/java/com/lzm/wanandroidwithlittleblackboxstyle/MainActivity.kt
+++ app/src/main/java/com/lzm/wanandroidwithlittleblackboxstyle/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.lzm.wanandroidwithlittleblackboxstyle
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+import com.lzm.wanandroidwithlittleblackboxstyle.view.TabButtomFactory
 
 class MainActivity : AppCompatActivity() {
+
+    val tabLayout:TabLayout by lazy {
+        findViewById(R.id.tablayout_cuttom_tab)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initButtomTsb()
+    }
+
+
+    fun initButtomTsb(){
+        val titleArray= arrayOf("首页","广场","我","广场")
+        val iconArray:IntArray= intArrayOf(R.drawable.home,R.drawable.square,R.drawable.project,R.drawable.account)
+        val tabButtomFactory=TabButtomFactory(this,tabLayout)
+        for(index in titleArray.indices){
+            tabLayout.addTab(tabButtomFactory.getTabItem(titleArray.get(index),iconArray.get(index)))
+        }
+
     }
 }
---
+++ app/src/main/java/com/lzm/wanandroidwithlittleblackboxstyle/view/TabButtomFactory.kt
@@ -0,0 +1,24 @@
+package com.lzm.wanandroidwithlittleblackboxstyle.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+import com.lzm.wanandroidwithlittleblackboxstyle.R
+
+class TabButtomFactory(val context:Context,val tabLayout: TabLayout) {
+
+    public fun getTabItem(title:String,icon:Int):TabLayout.Tab{
+        val view = LayoutInflater.from(context).inflate(R.layout.tab_item,null)
+        val image:ImageView= view.findViewById(R.id.icon)
+        val text:TextView = view.findViewById(R.id.title)
+        image.setBackgroundResource(icon)
+        text.setText(title)
+        val tab = tabLayout.newTab()
+        tab.setCustomView(view)
+        return tab
+    }
+}
